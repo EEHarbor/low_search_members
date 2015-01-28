76cbd21f767d6e5fce436e21dbbeb17af0e5f02d
Lodewijk Schutte <lodewijk@gmail.com>
2015-01-28 16:56:08 +0100

    First commit

---
+++ low_search_members/filters/members/lsf.members.php
@@ -0,0 +1,177 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Filter by member:key="val"
+ *
+ * @package        low_search
+ * @author         Lodewijk Schutte ~ Low <hi@gotolow.com>
+ * @link           https://github.com/low/low_search_members
+ * @copyright      Copyright (c) 2015, Low
+ */
+class Low_search_filter_members extends Low_search_filter {
+
+	/**
+	 * Prefix
+	 */
+	private $_pfx = 'member:';
+
+	/**
+	 * Allowed keys
+	 */
+	private $_keys = array(
+		'username',
+		'screen_name',
+		'email',
+		'url',
+		'location',
+		'occupation',
+		'interests',
+		'bday_d',
+		'bday_m',
+		'bday_y',
+		'aol_im',
+		'yahoo_im',
+		'msn_im',
+		'icq',
+		'bio',
+		'signature'
+	);
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Allows for member:key="val" parameters
+	 *
+	 * @access     private
+	 * @return     void
+	 */
+	public function filter($entry_ids)
+	{
+		// --------------------------------------
+		// Get member params
+		// --------------------------------------
+
+		$params = $this->params->get_prefixed($this->_pfx, TRUE);
+		$params = array_filter($params, 'low_not_empty');
+
+		// --------------------------------------
+		// Bail out of nothing's there
+		// --------------------------------------
+
+		if (empty($params))
+		{
+			return $entry_ids;
+		}
+
+		// --------------------------------------
+		// Log it
+		// --------------------------------------
+
+		$this->_log('Applying '.__CLASS__);
+
+		// --------------------------------------
+		// loop through params and collect where clauses
+		// --------------------------------------
+
+		$where = array();
+
+		foreach ($params AS $key => $val)
+		{
+			$search = FALSE;
+
+			// Search field?
+			if (strpos($key, 'search:') === 0)
+			{
+				$search = TRUE;
+				$key = substr($key, 7);
+				$val = $this->params->prep($key, $val);
+			}
+
+			// Skip unknown keys
+			if ( ! in_array($key, $this->_keys)) continue;
+
+			// Add prefix to key
+			$key = 'm.'.$key;
+
+			// Get where clause
+			if ($search)
+			{
+				$where[] = $this->_get_where_search($key, $val);
+			}
+			else
+			{
+				list($val, $in) = low_explode_param($val);
+				$where[] = array($key, ($in ? 'where_in' : 'where_not_in'), $val);
+			}
+		}
+
+		// --------------------------------------
+		// No valid params? Bail out
+		// --------------------------------------
+
+		if (empty($where))
+		{
+			$this->_log('No valid member filters found');
+			return $entry_ids;
+		}
+
+		// --------------------------------------
+		// Get channel IDs before starting the query
+		// --------------------------------------
+
+		$channel_ids = ee()->low_search_collection_model->get_channel_ids();
+
+		// --------------------------------------
+		// Start the query
+		// --------------------------------------
+
+		ee()->db->select('t.entry_id')
+		    ->distinct()
+		    ->from('channel_titles t')
+		    ->join('members m', 't.author_id = m.member_id');
+
+		// Existing member IDs?
+		if ($entry_ids)
+		{
+			ee()->db->where_in('t.entry_id', $entry_ids);
+		}
+
+		// Channel IDs?
+		if ($channel_ids)
+		{
+			ee()->db->where_in('t.channel_id', $channel_ids);
+		}
+
+		// Handle where clauses
+		foreach ($where AS $sql)
+		{
+			if (is_array($sql))
+			{
+				list($key, $method, $val) = $sql;
+				ee()->db->$method($key, $val);
+			}
+			else
+			{
+				ee()->db->where($sql, NULL, FALSE);
+			}
+		}
+
+		// --------------------------------------
+		// Get it
+		// --------------------------------------
+
+		$query = ee()->db->get();
+
+		// --------------------------------------
+		// Flatten it
+		// --------------------------------------
+
+		$entry_ids = low_flatten_results($query->result_array(), 'entry_id');
+
+		// --------------------------------------
+		// Return it
+		// --------------------------------------
+
+		return $entry_ids;
+	}
+}
---
+++ readme.md
@@ -0,0 +1,61 @@
+# Low Search Members for ExpressionEngine
+
+This add-on allows you to target standard member fields for searching with [Low Search](http://gotolow.com/addons/low-search). Channel entries returned will be limited by these members, as though the [author_id parameter](https://ellislab.com/expressionengine/user-guide/add-ons/channel/channel_entries.html#author-id) was set. This is particularly useful when dealing with a member add-on like [Zoo Visitor](http://ee-zoo.com/add-ons/visitor/). Requires Low Search v3.3.0+.
+
+## Installation
+
+- Download and unzip;
+- Copy the `low_search_members` folder to your `system/expressionengine/third_party` directory;
+- All set!
+
+## Usage
+
+Once installed, you can use these parameters, either in the Results tag or as fields in your search form:
+
+- `member:username`
+- `member:screen_name`
+- `member:email`
+- `member:url`
+- `member:location`
+- `member:occupation`
+- `member:interests`
+- `member:bday_d`
+- `member:bday_m`
+- `member:bday_y`
+- `member:aol_im`
+- `member:yahoo_im`
+- `member:msn_im`
+- `member:icq`
+- `member:bio`
+- `member:signature`
+
+To make these parameters behave like a [Field Search parameter](http://gotolow.com/addons/low-search/docs/filters#field-search), add `search:` after the `member:` prefix, eg. `member:search:username`. That will allow for partial matches.
+
+## Examples
+
+### Get entries for members that have a birthday in a given month
+
+    <select name="member:bday_m">
+        <option value="01">January</option>
+        <option value="02">February</option>
+        <option value="03">March</option>
+        <option value="04">April</option>
+        <option value="05">May</option>
+        <option value="06">June</option>
+        <option value="07">July</option>
+        <option value="08">August</option>
+        <option value="09">September</option>
+        <option value="10">October</option>
+        <option value="11">November</option>
+        <option value="12">December</option>
+    </select>
+
+### Get entries for members that have a gmail.com email address
+
+    <input name="member:search:email" value="@gmail.com">
+
+### Get entries for member that have selected items as interests
+
+    <input type="checkbox" name="member:search:interests[]" value="knitting"> Knitting
+    <input type="checkbox" name="member:search:interests[]" value="embroidery"> Embroidery
+    <input type="checkbox" name="member:search:interests[]" value="crochet"> Crochet
